7a06fe07b4f5f023d71f4ad693ffefa6247617df
Nikki Cerniuk <ncerniuk@gmail.com>
2025-08-27 12:29:10 -0700

    Create Bad3.java

---
+++ HW01/Bad3.java
@@ -0,0 +1,6 @@
+public class Bad3 {
+    public static void main(String[] args) {
+        char letter = 'a'; //changed type String to type char
+        System.out.println("letter is " + letter);
+    }
+}
