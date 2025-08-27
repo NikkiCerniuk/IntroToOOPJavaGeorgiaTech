b98682248ae00ffc3eed9bb3b407ed10ea183739
Nikki Cerniuk <ncerniuk@gmail.com>
2025-08-27 12:28:13 -0700

    Create Bad1.java

---
+++ HW01/Bad1.java
@@ -0,0 +1,7 @@
+public class Bad1 { //changed BAD to Bad
+    public static void main(String[] args) {
+        String str = new String("CS1331ROCKS");
+        System.out.println(str.length() - 5 + " is 11 - 5");
+    }
+}
+
