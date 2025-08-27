8ced555ecdd2d274c5f9a04987c07d44b493e0cb
Nikki Cerniuk <ncerniuk@gmail.com>
2025-08-27 12:43:21 -0700

    Create Crewmate.java

---
+++ HW05/Crewmate.java
@@ -0,0 +1,13 @@
+public interface Crewmate{
+    void completeTask(); //does not take in anything and does not return anything
+
+}
+/*
+
+//the above file is complete
+ Crewmate.java
+This file defines an interface with the name Crewmate.
+
+(Note: any class that implements Crewmate must provide a method 
+definition for this method)
+*/
